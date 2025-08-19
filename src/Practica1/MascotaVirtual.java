57448c4eaa86c8619a9fddf9d104828dc0a7d3f1
rma-toni <rmaflores.sw@gmail.com>
2025-08-19 17:52:13 -0300

    atributos de clase e instancia agregados -rma-toni - 19/08/25

--- src/Practica1/MascotaVirtual.java
+++ src/Practica1/MascotaVirtual.java
@@ -1,4 +1,13 @@
 package Practica1;
 
 public class MascotaVirtual {
+
+    public static final short MAX_ENERGIA = 100;
+    public static final short MIN_ENERGIA = 0;
+
+    private int energia;
+    private boolean dormido;
+    private int cantDesgaste;
+    private int cantComer;
+
 }
--- src/Practica1/Principal.java
+++
@@ -1,7 +0,0 @@
-package Practica1;
-
-public class Principal {
-    public static void main(String[] args) {
-
-    }
-}
