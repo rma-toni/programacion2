2099ca5bacf37a6754db9caa50912cfc4b62d178
rma-toni <rmaflores.sw@gmail.com>
2025-08-19 23:41:33 -0300

    update MascotaVirtual - rma-toni - 19/08/25

--- src/Practica1/MascotaVirtual.java
+++ src/Practica1/MascotaVirtual.java
@@ -10,4 +10,54 @@ public class MascotaVirtual {
     private int cantDesgaste;
     private int cantComer;
 
+    public MascotaVirtual(){
+        this.energia = MAX_ENERGIA;
+        this.dormido = false;
+        this.cantDesgaste = 0;
+        this.cantComer = 0;
+    }
+
+    //TODO comer():boolean
+
+    public boolean comer(){
+        boolean control = true;
+        if(estaVivo()){
+            cantComer++;
+            if(cantComer<5){
+                energia+=30;
+            }else{
+                //TODO morir?
+            }
+        }else{
+            control = false;
+        }
+        return  control;
+    }
+
+    //TODO beber():boolean
+
+    //TODO dormir():boolean
+
+    //TODO despertar():boolean
+
+    //TODO caminar():boolean
+
+    //TODO correr():boolean
+
+    //TODO saltar():boolean
+
+    //TODO obtenerEnergia():entero
+
+    //TODO estaDormido():boolean
+
+    //TODO obtenerCantDesgaste():entero
+
+    //TODO obtenerCantComer():entero
+
+    //TODO obtenerHumor():entero
+
+    //TODO estaVivo():boolean completar
+    boolean estaVivo(){
+        return true;
+    }
 }
