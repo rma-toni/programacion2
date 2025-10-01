5862cb5004b0ec60b35a4c2177805bd91cd904d9
rma-toni <rmaflores.sw@gmail.com>
2025-10-01 14:21:56 -0300

    FileManager update - Rogelio - 01/10/2025

--- src/TrabajoFinal/FileManager.java
+++ src/TrabajoFinal/FileManager.java
@@ -1,8 +1,24 @@
 package TrabajoFinal;
 
+import java.io.*; //Solo necesitamos ObjectInputStream y FileInputStream
 
 public class FileManager {
-    public static <T> void saveData(T args){
 
+    public static <T extends Serializable> void saveData(T object){
+        //ObjectOutputStream convierte el objeto en una secuencia de bits
+        //FileOutputStream permite escribir bytes en un archivo
+        //Se crea el ObjectOutputStream para que al salir del try java cierre automaticamente lo que se instancio entre parentesis
+        try (ObjectOutputStream fileStream = new ObjectOutputStream(new FileOutputStream("data.dat"))) {
+            fileStream.writeObject(object);
+            System.out.println("Guardado correcto"); //TODO DEBUG
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
+
+    public static <T> T loadData(){
+
+        return null;
+    }
+
 }
